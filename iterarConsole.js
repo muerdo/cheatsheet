93de2c30809b046f36150f139f91db7c7bc0f910
muerdo <130400733+muerdo@users.noreply.github.com>
2024-02-04 21:12:58 -0300

    Create iterarConsole.js
    as

---
+++ iterarConsole.js
@@ -0,0 +1,7 @@
+// Selecione todos os elementos <div> com a classe "component__feedback-content"
+var elementos = document.querySelectorAll<('div.component__feedback-content')>;
+
+// Itere sobre os elementos e imprima-os no console
+elementos.forEach(function(elemento) {
+    console.log(elemento);
+});
