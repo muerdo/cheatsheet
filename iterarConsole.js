253172d2b095eba2e22fa5c1ab13c241cc65222c
muerdo <130400733+muerdo@users.noreply.github.com>
2024-02-23 20:09:32 -0300

    Update iterarConsole.js

--- iterarConsole.js
+++ iterarConsole.js
@@ -1,7 +1,7 @@
-// Selecione todos os elementos <div> com a classe "component__feedback-content"
-var elementos = document.querySelectorAll<('div.component__feedback-content')>;
 
-// Itere sobre os elementos e imprima-os no console
+var elementos = document.querySelectorAll('div.component__feedback-content');
+
+
 elementos.forEach(function(elemento) {
     console.log(elemento);
 });
